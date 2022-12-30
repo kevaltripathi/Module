e1617630bc8d1b95ce5a516e3e910d840dc362ea
Keval Tripathi <keval@tripathi.ca>
2022-12-30 13:43:10 -0500

    Added Wifi Support

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -1,6 +1,8 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include <WiFiMulti.h>
+#include <HTTPClient.h>
 
 #define SERVICE_UUID "19B10000-E8F2-537E-4F6C-D104768A1214"
 #define CHARACTERISTIC_UUID "19B10001-E8F2-537E-4F6C-D104768A1214"
@@ -10,6 +12,7 @@ bool deviceConnected = false;
 bool written = false;
 bool led = false;
 
+WiFiMulti WiFiMulti;
 BLECharacteristic *pCharacteristic;
 
 class ServerCallbacks : public BLEServerCallbacks {
@@ -31,15 +34,41 @@ public:
   }
 };
 
+void bluetooth() {
+  if (deviceConnected && written) {
+    if (led) {
+      Serial.println("LED on");
+      digitalWrite(ledPin, HIGH);
+      written = false;
+    } else {
+      Serial.println("LED off");
+      digitalWrite(ledPin, LOW);
+      written = false;
+    }
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   while (!Serial);
 
-  Serial.println("Starting BLE work!");
+  WiFiMulti.addAP("SSID", "PW");
+
+  Serial.print("Waiting for WiFi... ");
+
+  while(WiFiMulti.run() != WL_CONNECTED) {
+      Serial.print(".");
+      delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 
   pinMode(ledPin, OUTPUT);
 
-  BLEDevice::init("LED");
+  BLEDevice::init("Module BLE");
   BLEServer *pServer = BLEDevice::createServer();
   pServer->setCallbacks(new ServerCallbacks());
   BLEService *pService = pServer->createService(SERVICE_UUID);
@@ -55,19 +84,30 @@ void setup() {
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(true);
   BLEDevice::startAdvertising();
-  Serial.println("Characteristic defined! Now you can read it in your phone!");
 }
 
 void loop() {
-  if (deviceConnected && written) {
-    if (led) {
-      Serial.println("LED on");
-      digitalWrite(ledPin, HIGH);
-      written = false;
-    } else {
-      Serial.println("LED off");
-      digitalWrite(ledPin, LOW);
-      written = false;
+
+  bluetooth();
+  
+  HTTPClient http;
+
+  http.begin("http://api.DOMAIN/light");
+
+  int httpCode = http.GET();
+  
+  if(httpCode > 0) {
+    Serial.println(httpCode);
+    if(httpCode == HTTP_CODE_OK) {
+      String payload = http.getString();
+      Serial.println(payload);
     }
+  } else {
+    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
   }
+  
+  http.end();
+
+  Serial.println("Waiting 5 seconds before restarting...");
+  delay(5000);
 }
