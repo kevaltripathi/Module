1f9c76e584d0b91fca7959ed6a9fbf0f5443d88d
Keval Tripathi <keval@tripathi.ca>
2023-03-05 16:19:48 +1000

    Added db to webserver

--- WebServer/webserver.py
+++ WebServer/webserver.py
@@ -1,32 +1,56 @@
 import wsgiref.util
 import json
+import MySQLdb
 
-Matrix = ["", "", "", "", "", "", "", ""]
-Title = ""
-Message = {"lineQty": "", "lines": [], "weight": []}
+
+def db_commit(column, value):
+    try:
+        connection = MySQLdb.connect('localhost', 'user', 'password', 'database')
+        with connection:
+            cursor = connection.cursor()
+            sql = "UPDATE `Data` SET `{0}` = '{1}' WHERE `id` = 1".format(column, value)
+            cursor.execute(sql)
+            cursor.close()
+            connection.commit()
+        return "200 OK", "Success"
+    except Exception as e:
+        return "500 Internal Server Error", e
+
+
+def db_get(column):
+    try:
+        connection = MySQLdb.connect('localhost', 'user', 'password', 'database')
+        with connection:
+            cursor = connection.cursor()
+            sql = "SELECT `{0}` FROM `Data` WHERE `id` = 1".format(column)
+            cursor.execute(sql)
+            result = cursor.fetchone()
+            cursor.close()
+            return result[0]
+    except Exception as e:
+        return e
 
 
 def webserver(environ, start_response):
-    global Matrix, Title, Message
     uri = wsgiref.util.request_uri(environ)
     path = uri.split("/")[3]
 
     status = ""
-    message = ""
+    response = ""
 
     if environ['REQUEST_METHOD'] == 'GET':
         if path.lower() == "matrix":
             status = "200 OK"
-            message = json.dumps(Matrix)
+            response = json.dumps(db_get("matrix"))
         elif path.lower() == "title":
             status = "200 OK"
-            message = Title
+            response = db_get("title")
         elif path.lower() == "message":
             status = "200 OK"
-            message = json.dumps(Message)
+            response = json.dumps(db_get("message"))
         else:
             status = "404 Not Found"
-            message = "Page Not Found"
+            response = "Page Not Found"
 
     elif environ['REQUEST_METHOD'] == 'POST':
         try:
@@ -34,32 +58,28 @@ def webserver(environ, start_response):
             body = str(environ['wsgi.input'].read(size))
 
             if path.lower() == "matrix":
-                Matrix = body.strip(" []\n").split(',', 7)
-                status = "200 OK"
-                message = "Success"
+                status, response = db_commit("matrix", body.strip(" []\n").split(',', 7))
             elif path.lower() == "title":
-                Title = body
-                status = "200 OK"
-                message = "Success"
+                status, response = db_commit("title", body.strip("\n"))
             elif path.lower() == "message":
+                message = {"lineQty": "", "lines": [], "weight": []}
                 split = body.strip(" []\n").split(',')
-                Message["lineQty"] = split[0]
+                message["lineQty"] = split[0]
                 lines = []
                 weight = []
                 for x in range(int(split[0])):
                     lines.append(split[x + 1])
                     weight.append(split[x + int(split[0]) + 1])
-                Message["lines"] = lines
-                Message["weight"] = weight
-                status = "200 OK"
-                message = "Success"
+                message["lines"] = lines
+                message["weight"] = weight
+                status, response = db_commit("message", message)
             else:
                 status = "404 Not Found"
-                message = "Page Not Found"
+                response = "Page Not Found"
         except Exception as e:
             status = "500 Internal Server Error"
-            message = e
+            response = e
 
     headers = [('Content-type', 'text/plain')]
     start_response(status, headers)
-    return [str(message)]
+    return [str(response)]
